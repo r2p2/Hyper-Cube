c4be25b08a360e57742292235fd8c5f6fd6e42a6
Robert Peters <robert-peters@gmx.net>
2011-03-20 23:55:42 +0100

    first step for polymophism
    introduced super class for all 3D objects
    also switched the commit language to english

---
+++ Object3D.cpp
@@ -0,0 +1,19 @@
+/*
+ * Object3D.cpp
+ *
+ *  Created on: 20.03.2011
+ *      Author: r2p2
+ */
+
+#include "Object3D.h"
+
+Object3D::Object3D(double center_x, double center_y, double center_z)
+:center_x(center_x)
+,center_y(center_y)
+,center_z(center_z)
+{
+}
+
+Object3D::~Object3D()
+{
+}
---
+++ Object3D.h
@@ -0,0 +1,28 @@
+/*
+ * Object3D,h
+ *
+ * Created on: 20.03.2011
+ *     Author: r2p2
+ */
+
+#ifndef OBJECT3D_H_
+#define OBJECT3D_H_
+
+class Object3D
+{
+public:
+  Object3D(double center_x, double center_y, double center_z);
+  virtual ~Object3D();
+
+  virtual void rotate_x(double angle) = 0;
+  virtual void rotate_y(double angle) = 0;
+  virtual void rotate_z(double angle) = 0;
+  virtual void move_(double x_delta, double y_delta, double z_delta) = 0;
+
+protected:
+  double center_x;
+  double center_y;
+  double center_z;
+};
+
+#endif
