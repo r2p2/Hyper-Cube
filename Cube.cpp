8f9c3b39ad484ba4e59b01d0550320b33ac4feb6
Robert Peters <robert-peters@gmx.net>
2011-03-22 20:17:27 +0100

    no need for dynamic allocated Points

--- Cube.cpp
+++ Cube.cpp
@@ -10,33 +10,29 @@
 Cube::Cube()
 :Object3D()
 {
-  Point *points[8];
-  points[0] = new Point(-0.9, -0.9, +0.9);
-  points[1] = new Point(+0.9, -0.9, +0.9);
-  points[2] = new Point(+0.9, +0.9, +0.9);
-  points[3] = new Point(-0.9, +0.9, +0.9);
-  points[4] = new Point(-0.9, -0.9, -0.9);
-  points[5] = new Point(+0.9, -0.9, -0.9);
-  points[6] = new Point(+0.9, +0.9, -0.9);
-  points[7] = new Point(-0.9, +0.9, -0.9);
+  Point points[8] = {
+    Point(-0.9, -0.9, +0.9),
+    Point(+0.9, -0.9, +0.9),
+    Point(+0.9, +0.9, +0.9),
+    Point(-0.9, +0.9, +0.9),
+    Point(-0.9, -0.9, -0.9),
+    Point(+0.9, -0.9, -0.9),
+    Point(+0.9, +0.9, -0.9),
+    Point(-0.9, +0.9, -0.9)
+  };
 
-  lines[0] = new Line(*points[0], *points[1]);
-  lines[1] = new Line(*points[1], *points[2]);
-  lines[2] = new Line(*points[2], *points[3]);
-  lines[3] = new Line(*points[0], *points[3]);
-  lines[4] = new Line(*points[4], *points[5]);
-  lines[5] = new Line(*points[5], *points[6]);
-  lines[6] = new Line(*points[6], *points[7]);
-  lines[7] = new Line(*points[7], *points[4]);
-  lines[8] = new Line(*points[0], *points[4]);
-  lines[9] = new Line(*points[1], *points[5]);
-  lines[10] = new Line(*points[2], *points[6]);
-  lines[11] = new Line(*points[3], *points[7]);
-
-  for(int i = 0; i < 8; i++)
-  {
-    delete points[i];
-  }
+  lines[0] = new Line(points[0], points[1]);
+  lines[1] = new Line(points[1], points[2]);
+  lines[2] = new Line(points[2], points[3]);
+  lines[3] = new Line(points[0], points[3]);
+  lines[4] = new Line(points[4], points[5]);
+  lines[5] = new Line(points[5], points[6]);
+  lines[6] = new Line(points[6], points[7]);
+  lines[7] = new Line(points[7], points[4]);
+  lines[8] = new Line(points[0], points[4]);
+  lines[9] = new Line(points[1], points[5]);
+  lines[10] = new Line(points[2], points[6]);
+  lines[11] = new Line(points[3], points[7]);
 }
 
 Cube::~Cube()
